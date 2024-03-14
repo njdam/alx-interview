e74f06cddacc074d9a34f3ac4a246e9478de9c1c
njdam <jeandamngaboyimana@gmail.com>
2024-03-14 12:54:46 +0000

    Alx Interview 0x01. Lockboxes!

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Alx-interview for determining if all the boxes can be opened
+""" Alx-interview for determining if all the boxes can be opened """
 
 
 def canUnlockAll(boxes):
@@ -7,25 +7,22 @@ def canUnlockAll(boxes):
     Determines if all the boxes can be opened.
 
     Args:
-    - boxes: A list of lists representing the boxes.
+        - boxes: A list of lists representing the boxes.
 
     Returns:
-    - True if all boxes can be opened, else False.
+        - True if all boxes can be opened, else False.
     """
     canUnlockAll = False
     keys = {0: True}
-
-    n_boxes = len(boxes)
-
-    while (True):
+    n = len(boxes)
+    while True:
 
         n_keys = len(keys)
 
-        # Iterate to check boxes if have all keys which can unlock all
-        for i in range(len(boxes)):
+        for i in range(n):
             if boxes[i] and keys.get(i, False):
                 for j in boxes[i]:
-                    if j < n_boxes:
+                    if j < n:
                         keys[j] = True
                     boxes[i] = None
 
@@ -35,5 +32,4 @@ def canUnlockAll(boxes):
     if n_keys == len(boxes):
         canUnlockAll = True
 
-    # If all boxes have been visited, return True.
     return canUnlockAll
